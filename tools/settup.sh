b4537b21589a08b0464fc4ade07d98086c2c4ee0
ShotaIuchi <shotaiuchi.develop@gmail.com>
2023-12-12 13:15:58 +0900

    [前準備]gitconfig

---
+++ tools/settup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##### 必須 #####
+
+### gitの設定 ###
+# コメント先頭に#を使えるようにする
+# ※コメント先頭に;は使えなくなる（-mオプションでのみ使える）
+git config core.commentChar ';'
+
+##### オプショナル #####
+### gitの設定 ###
+# git config core.quotePath false
+# git config core.ignorecase false
+# git config core.autocrlf true
+# git config core.autoCRLF false
+# git config core.fileMode false
+# git config core.symlinks true
