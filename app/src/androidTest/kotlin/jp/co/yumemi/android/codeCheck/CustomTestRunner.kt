07e40f0c9bd5c5a47614420f6aed0753e9539593
ShotaIuchi <shotaiuchi.develop@gmail.com>
2023-12-12 23:31:09 +0900

    テストを追加(#7)::AndroidTestコード追加

---
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/codeCheck/CustomTestRunner.kt
@@ -0,0 +1,16 @@
+package jp.co.yumemi.android.codeCheck
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+class CustomTestRunner : AndroidJUnitRunner() {
+    @Throws(
+        InstantiationException::class,
+        IllegalAccessException::class,
+        ClassNotFoundException::class
+    )
+    override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
+        return super.newApplication(cl, TestApplication::class.java.name, context)
+    }
+}
