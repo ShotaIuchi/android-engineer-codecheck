f9359c43f2e77941cf4cd5e1158cfd8522f95e03
ShotaIuchi <shotaiuchi.develop@gmail.com>
2023-12-12 22:45:22 +0900

    テストを追加(#7)::UnitTestで発覚した問題修正
    - 未キャッチエクセプション捕捉処理追加
    	- ContentConvertException

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/data/repository/GitHubRepositoryImpl.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/data/repository/GitHubRepositoryImpl.kt
@@ -16,6 +16,7 @@ class GitHubRepositoryImpl(private val gitHubDataSource: GitHubDataSource) : Git
      * @throws io.ktor.client.plugins.ClientRequestException クライアント側のリクエストエラーが発生した場合
      * @throws io.ktor.client.plugins.HttpRequestTimeoutException サーバーからの応答エラーが発生した場合
      * @throws io.ktor.client.plugins.ServerResponseException HTTPリクエストがタイムアウトした場合
+     * @throws io.ktor.serialization.ContentConvertException JSON変換に失敗した場合
      * @throws io.ktor.utils.io.errors.IOException 入出力エラーが発生した場合
      */
     override suspend fun searchRepository(inputText: String): List<GitHubRepoModel> =
